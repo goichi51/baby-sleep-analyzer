3bb231a958d6cd2f85b28af0b8f0a883931ba7a5
goichi51 <forpg50@gmail.com>
2026-08-04 17:57:41 +0900

    表示を整える

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import type { Log } from '@/type/log'
+import { format } from 'date-fns'
+import { computed } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   log: Log
 }>()
+
+const events = computed(() =>
+  props.log.events.map((e) => ({
+    時刻: format(e.datetime, 'yyyy-MM-dd HH:mm'),
+    出来事: e.name,
+    メモ: e.memo,
+  })),
+)
 </script>
 <template>
   <v-card variant="tonal" title="一日の記録">
     <v-card-text>
-      <v-data-table class="border-b-sm" hide-default-footer :items="log.events ?? []" />
+      <v-data-table class="border-b-sm" hide-default-footer :items="events" />
       <div class="mt-4" v-if="log.diary">
         <div class="mb-2 text-title-medium font-weight-bold">日記</div>
         {{ log.diary.text }}
