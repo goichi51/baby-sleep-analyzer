f408b0c5f5194aeb11c315857cdb331d141f24f6
forpg50@goichi51 <forpg50@gmail.com>
2026-08-28 14:51:08 +0900

    vitest関連設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -26,12 +26,11 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     proxy: {
-      // フロントから /api/xxx にアクセスしたら、Honoサーバー(port 3000)に飛ばす
       '/api': 'http://localhost:3000',
     },
     watch: {
-      usePolling: true
-    }
+      usePolling: true,
+    },
   },
   resolve: {
     alias: {
